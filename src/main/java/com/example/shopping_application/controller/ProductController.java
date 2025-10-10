173f8e3e4aa54e7fa5cf1445c31674f54f3309f8
SonMyeongJin <smj2802@naver.com>
2025-10-10 16:42:48 +0900

    controller - service 연결

--- src/main/java/com/example/shopping_application/controller/ProductController.java
+++ src/main/java/com/example/shopping_application/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.shopping_application.controller;
 
 import com.example.shopping_application.domain.Product;
+import com.example.shopping_application.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+
+    private ProductService productService;
+
+    // 생섲아를 이용하여 생성될때 자동으로 @Autowired 를 통해 Service와 연결
+    @Autowired
+    ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
     public Product creatProduct(@RequestBody Product product) {
