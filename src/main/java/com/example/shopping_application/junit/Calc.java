77ec816db6914662fcbdd441db2e078f95a74ab3
SonMyeongJin <smj2802@naver.com>
2025-10-14 11:03:52 +0900

    test: add asertAll()

--- src/main/java/com/example/shopping_application/junit/Calc.java
+++ src/main/java/com/example/shopping_application/junit/Calc.java
@@ -3,6 +3,7 @@ package com.example.shopping_application.junit;
 public class Calc {
 
     public int add(int a, int b) {
-        return a * b;
+        //return a * b;
+        return a + b;
     }
 }
--- src/test/java/com/example/shopping_application/junit/CalcTest.java
+++ src/test/java/com/example/shopping_application/junit/CalcTest.java
@@ -4,12 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
     @org.junit.jupiter.api.Test
-    void add() {
+    void addTest() {
 
+        // 성공하는 테스트 코드
         assertEquals(4, new Calc().add(2, 2));
-        assertEquals(6, new Calc().add(4, 2));
 
-        assertThrows(NumberFormatException.class, () -> new Calc().add(-100, 10),
-                "음수는 나오지 않습니다");
+        // 실패하는 테스트 코드
+        //assertEquals(6, new Calc().add(4, 2));
+
+        // 예외가 발생하는 테스트 코드
+        //assertThrows(NumberFormatException.class, () -> new Calc().add(-100, 10), "음수는 나오지 않습니다");
+
+        // 여러개의 테스트를 한번에 실행
+        assertAll(() -> assertEquals(4,new Calc().add(2, 2),"두 수의 합은 4입니다"),
+                () -> assertEquals(6,new Calc().add(2, 4),"두 수의 합은 6입니다") );
+
     }
 }
