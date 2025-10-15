3c90ecfbbb0c8eed93da0953cb5742958c972ce5
SonMyeongJin <smj2802@naver.com>
2025-10-15 15:44:47 +0900

    [Test: money calc] 3.generation method for test

--- src/main/java/com/example/shopping_application/test_practice/money/Dollar.java
+++ src/main/java/com/example/shopping_application/test_practice/money/Dollar.java
@@ -5,8 +5,10 @@ public class Dollar {
     int amount;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
     void times(int multiplier) {
+        amount = amount * multiplier;
     }
 
 }
