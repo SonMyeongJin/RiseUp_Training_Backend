0f7ad2eccbbb272e6ca8859837f37e847691bcd4
SonMyeongJin <smj2802@naver.com>
2025-10-16 12:22:46 +0900

    [Test: TDD] 12. Generate (Franc.java) for test method

---
+++ TDD_application/src/main/java/com/example/tdd/money/Franc.java
@@ -0,0 +1,23 @@
+package com.example.tdd.money;
+
+public class Franc {
+
+    private int amount;
+
+    // 생성자로 amount 초기화
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    // 곱셈 메서드 구현
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    // equals 메서드 오버라이딩
+    @Override
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
