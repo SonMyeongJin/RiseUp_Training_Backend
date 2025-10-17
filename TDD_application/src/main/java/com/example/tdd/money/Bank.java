4433a0ffd8fd6701851e291ac382b95ddde6e3d4
SonMyeongJin <smj2802@naver.com>
2025-10-17 18:09:52 +0900

    [Test: TDD] 23. Refactoring code to organize Sum or Bank role

--- TDD_application/src/main/java/com/example/tdd/money/Bank.java
+++ TDD_application/src/main/java/com/example/tdd/money/Bank.java
@@ -2,6 +2,9 @@ package com.example.tdd.money;
 
 public class Bank {
     Money reduce (Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+//        int amount = sum.augend.amount + sum.addend.amount;
+//        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }
---
+++ TDD_application/src/main/java/com/example/tdd/money/Sum.java
@@ -0,0 +1,15 @@
+package com.example.tdd.money;
+
+public class Sum implements Expression {
+    Money augend;
+    Money addend;
+    Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
+}
