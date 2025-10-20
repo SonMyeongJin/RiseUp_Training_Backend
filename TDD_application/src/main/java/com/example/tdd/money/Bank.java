5e69de7d0b41e6f0d2d4e9d0656fa61b44f41aea
SonMyeongJin <smj2802@naver.com>
2025-10-20 11:53:18 +0900

    [Test: TDD] 28. Refactoring code to give a role

--- TDD_application/src/main/java/com/example/tdd/money/Bank.java
+++ TDD_application/src/main/java/com/example/tdd/money/Bank.java
@@ -2,19 +2,20 @@ package com.example.tdd.money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-
-        if (source instanceof Money) {
-            return ((Money) source).reduce(to);
-        }
-
-        Sum sum = (Sum) source;
+//        if (source instanceof Money) {
+//            return ((Money) source).reduce(to);
+//        }
+//        Sum sum = (Sum) source;
 //        int amount = sum.augend.amount + sum.addend.amount;
 //        return new Money(amount, to);
-
-        return source.reduce(to);
+        return source.reduce(this,to);
     }
 
     void addRate(String from, String to, int rate) {
 
+    }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     }
 }
