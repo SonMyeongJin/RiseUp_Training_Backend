84b17dd63651b913f06385536f0587731cfd8463
SonMyeongJin <smj2802@naver.com>
2025-10-20 12:13:08 +0900

    [Test: TDD] 29. Generate test

--- TDD_application/src/main/java/com/example/tdd/money/Bank.java
+++ TDD_application/src/main/java/com/example/tdd/money/Bank.java
@@ -1,6 +1,10 @@
 package com.example.tdd.money;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
     Money reduce(Expression source, String to) {
 //        if (source instanceof Money) {
 //            return ((Money) source).reduce(to);
@@ -12,10 +16,12 @@ public class Bank {
     }
 
     void addRate(String from, String to, int rate) {
-
+        rates.put(new Pair(from, to), rate);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if (from.equals(to))
+            return 1;
+        return rates.get(new Pair(from, to));
     }
 }
