4e73ed73488d3534cf8dbd5350f73b0f4cb51cbf
SonMyeongJin <smj2802@naver.com>
2025-10-21 15:22:51 +0900

    [Feat: DB] 3. Connect Mysql by JDBC

--- shopping_application/src/main/java/com/example/shopping_application/ShoppingApplication.java
+++ shopping_application/src/main/java/com/example/shopping_application/ShoppingApplication.java
@@ -1,9 +1,14 @@
 package com.example.shopping_application;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootApplication
 public class ShoppingApplication {
@@ -19,5 +24,14 @@ public class ShoppingApplication {
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setFieldMatchingEnabled(true);
         return new ModelMapper();
+    }
+
+    @Bean
+    public ApplicationRunner runner(DataSource dataSource) {
+        return args -> {
+
+            Connection connection = dataSource.getConnection();
+            System.out.println("DataSource = " + dataSource);
+        };
     }
 }
