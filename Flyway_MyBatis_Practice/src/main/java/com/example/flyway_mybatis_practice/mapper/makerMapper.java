39328cc8c594e642bf56a6f23b918c90ccc89270
SonMyeongJin <smj2802@naver.com>
2025-10-23 16:33:43 +0900

    [Feat: Mybatis] 4. Add Mapper (products,maker)

---
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/mapper/makerMapper.java
@@ -0,0 +1,8 @@
+package com.example.flyway_mybatis_practice.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class makerMapper {
+
+}
---
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/mapper/productsMapper.java
@@ -0,0 +1,7 @@
+package com.example.flyway_mybatis_practice.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class productsMapper {
+}
