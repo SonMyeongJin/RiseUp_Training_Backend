dfb75bbfacb7b8b87e7c90759058eff15304a086
SonMyeongJin <smj2802@naver.com>
2025-10-23 14:07:38 +0900

    Create New Module application ( Topic : Flyway,MyBatis Practice )

---
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/FlywayMyBatisPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyway_mybatis_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayMyBatisPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayMyBatisPracticeApplication.class, args);
+    }
+
+}
---
+++ Flyway_MyBatis_Practice/src/test/java/com/example/flyway_mybatis_practice/FlywayMyBatisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flyway_mybatis_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayMyBatisPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
