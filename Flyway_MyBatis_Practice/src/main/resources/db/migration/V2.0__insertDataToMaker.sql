27af9f92d9f2d161555a3d2a54bbec28a79c3796
SonMyeongJin <smj2802@naver.com>
2025-10-23 17:31:41 +0900

    [Feat: Flyway] 5. Insert data by sql (Answert to Q1,Q2)

---
+++ Flyway_MyBatis_Practice/src/main/resources/db/migration/V2.0__insertDataToMaker.sql
@@ -0,0 +1,4 @@
+INSERT INTO `dtb_maker` (maker_id, name, update_date, del_flg)
+VALUES (1, '제조사A', NULL, 0),
+       (2, '제조사B', CURRENT_TIMESTAMP, 0),
+       (3, '제조사C', '2025-10-23 08:00:00', 0);
---
+++ Flyway_MyBatis_Practice/src/main/resources/db/migration/V2.1__insertDataToProducts.sql
@@ -0,0 +1,4 @@
+INSERT INTO `dtb_products` (product_id, name, name_kana, maker_id, status, price01)
+VALUES (1001, '상품1', '상풍1', 1, 2, 10000),
+       (1002, '상품2', '상풍2', 2, 1, 25000),
+       (1003, '상품3', '상풍3', NULL, 2, NULL);
