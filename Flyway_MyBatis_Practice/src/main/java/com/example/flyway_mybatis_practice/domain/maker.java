f9298e1dba5709dc076e2ab687fb37d806b270b1
SonMyeongJin <smj2802@naver.com>
2025-10-24 11:15:22 +0900

    [Feat: Mybatis] 8. Add Getter&Setter of Domain

--- Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/maker.java
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/maker.java
@@ -11,4 +11,44 @@ public class maker {
     Timestamp create_time;
     Timestamp update_time;
     int del_flg;
+
+    public int getMaker_id() {
+        return maker_id;
+    }
+
+    public void setMaker_id(int maker_id) {
+        this.maker_id = maker_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Timestamp create_time) {
+        this.create_time = create_time;
+    }
+
+    public Timestamp getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Timestamp update_time) {
+        this.update_time = update_time;
+    }
+
+    public int getDel_flg() {
+        return del_flg;
+    }
+
+    public void setDel_flg(int del_flg) {
+        this.del_flg = del_flg;
+    }
 }
--- Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/products.java
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/products.java
@@ -9,4 +9,44 @@ public class products {
     String name_kana;
     int maker_id;
     int price01;
+
+    public int getProducts_id() {
+        return products_id;
+    }
+
+    public void setProducts_id(int products_id) {
+        this.products_id = products_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName_kana() {
+        return name_kana;
+    }
+
+    public void setName_kana(String name_kana) {
+        this.name_kana = name_kana;
+    }
+
+    public int getMaker_id() {
+        return maker_id;
+    }
+
+    public void setMaker_id(int maker_id) {
+        this.maker_id = maker_id;
+    }
+
+    public int getPrice01() {
+        return price01;
+    }
+
+    public void setPrice01(int price01) {
+        this.price01 = price01;
+    }
 }
