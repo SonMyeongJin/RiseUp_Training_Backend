e09d75c03dc53d18f9a4800312e99e37a8c72467
SonMyeongJin <smj2802@naver.com>
2025-10-24 18:13:54 +0900

    [Feat: Mybatis] 14. Add domains for order system

---
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/Item.java
@@ -0,0 +1,51 @@
+package com.example.flyway_mybatis_practice.domain;
+
+public class Item {
+    int order_item_id;
+    int orders_id;
+    String item_name;
+    int price;
+    int quantity;
+
+    public int getOrder_item_id() {
+        return order_item_id;
+    }
+
+    public void setOrder_item_id(int order_item_id) {
+        this.order_item_id = order_item_id;
+    }
+
+    public int getOrders_id() {
+        return orders_id;
+    }
+
+    public void setOrders_id(int orders_id) {
+        this.orders_id = orders_id;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
+
+
---
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/Orders.java
@@ -0,0 +1,40 @@
+package com.example.flyway_mybatis_practice.domain;
+
+public class Orders {
+    int orders_id;
+    int customer_id;
+    int total_price;
+    int total_amount;
+
+    public int getOrders_id() {
+        return orders_id;
+    }
+
+    public void setOrders_id(int orders_id) {
+        this.orders_id = orders_id;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(int total_price) {
+        this.total_price = total_price;
+    }
+
+    public int getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(int total_amount) {
+        this.total_amount = total_amount;
+    }
+}
