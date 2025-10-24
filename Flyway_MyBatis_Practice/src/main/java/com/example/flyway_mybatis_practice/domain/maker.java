16deafb272e7011309273d5bac307fc4d67162a3
SonMyeongJin <smj2802@naver.com>
2025-10-24 11:57:35 +0900

    [Refactor: Mybatis] 10. edit domain,application.properties

--- Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/maker.java
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/maker.java
@@ -1,54 +1,26 @@
 package com.example.flyway_mybatis_practice.domain;
 
-import lombok.Data;
+import java.sql.Timestamp;
 
-import java.security.Timestamp;
-
-@Data
 public class maker {
-    int maker_id;
-    String name;
-    Timestamp create_time;
-    Timestamp update_time;
-    int del_flg;
+    private int maker_id;
+    private String name;
+    private java.sql.Timestamp create_date;
+    private java.sql.Timestamp update_date;
+    private int del_flg;
 
-    public int getMaker_id() {
-        return maker_id;
-    }
+    public int getMaker_id() { return maker_id; }
+    public void setMaker_id(int maker_id) { this.maker_id = maker_id; }
 
-    public void setMaker_id(int maker_id) {
-        this.maker_id = maker_id;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public java.sql.Timestamp getCreate_date() { return create_date; }
+    public void setCreate_date(java.sql.Timestamp create_date) { this.create_date = create_date; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public java.sql.Timestamp getUpdate_date() { return update_date; }
+    public void setUpdate_date(Timestamp update_date) { this.update_date = update_date; }
 
-    public Timestamp getCreate_time() {
-        return create_time;
-    }
-
-    public void setCreate_time(Timestamp create_time) {
-        this.create_time = create_time;
-    }
-
-    public Timestamp getUpdate_time() {
-        return update_time;
-    }
-
-    public void setUpdate_time(Timestamp update_time) {
-        this.update_time = update_time;
-    }
-
-    public int getDel_flg() {
-        return del_flg;
-    }
-
-    public void setDel_flg(int del_flg) {
-        this.del_flg = del_flg;
-    }
+    public int getDel_flg() { return del_flg; }
+    public void setDel_flg(int del_flg) { this.del_flg = del_flg; }
 }
--- Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/products.java
+++ Flyway_MyBatis_Practice/src/main/java/com/example/flyway_mybatis_practice/domain/products.java
@@ -1,14 +1,11 @@
 package com.example.flyway_mybatis_practice.domain;
 
-import lombok.Data;
-
-@Data
 public class products {
-    int products_id;
-    String name;
-    String name_kana;
-    int maker_id;
-    int price01;
+    private int products_id;
+    private String name;
+    private String name_kana;
+    private int maker_id;
+    private int price01;
 
     public int getProducts_id() {
         return products_id;
