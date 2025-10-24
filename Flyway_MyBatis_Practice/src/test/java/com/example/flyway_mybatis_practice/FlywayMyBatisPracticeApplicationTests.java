ed5c59d408266b5e2363f54681c07e9836337e45
SonMyeongJin <smj2802@naver.com>
2025-10-24 15:16:45 +0900

    [Feat: Mybatis] 12. Test to change status=1 -> status=2

--- Flyway_MyBatis_Practice/src/test/java/com/example/flyway_mybatis_practice/FlywayMyBatisPracticeApplicationTests.java
+++ Flyway_MyBatis_Practice/src/test/java/com/example/flyway_mybatis_practice/FlywayMyBatisPracticeApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.flyway_mybatis_practice;
 
+
+import com.example.flyway_mybatis_practice.mapper.productsMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FlywayMyBatisPracticeApplicationTests {
+class FlywayMybatisApplicationTests {
+
+    @Autowired
+    productsMapper productsMapper;
 
     @Test
-    void contextLoads() {
+    void updateStatus2ById() {
+        Long id = 1002L;
+        int affected = productsMapper.updateStatus2ById(id);
+        System.out.println("압데이트 = " + affected);
     }
-
 }
