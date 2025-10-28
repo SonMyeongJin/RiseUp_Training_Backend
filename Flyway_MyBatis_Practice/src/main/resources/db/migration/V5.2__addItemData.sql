3b6e048909da8e36a4c1b1a4dbcd4d3cdcf33575
SonMyeongJin <smj2802@naver.com>
2025-10-28 10:55:00 +0900

    [Feat: Flyway] 28. Add item data for 'getOrdersDataMinItems'

---
+++ Flyway_MyBatis_Practice/src/main/resources/db/migration/V5.2__addItemData.sql
@@ -0,0 +1,25 @@
+-- order_id = 1
+INSERT INTO order_item (order_item_id, orders_id, item_name, price, quantity)
+VALUES (16, 10, 'RiseUp Coffee', 5000, 2),
+       (3, 10, 'Brazil Single', 4500, 1),
+       (4, 10, 'Espresso Shot', 3000, 2);
+
+-- order_id = 2
+INSERT INTO order_item (order_item_id, orders_id, item_name, price, quantity)
+VALUES (21, 11, 'RiseUp Milk', 8000, 4),
+       (5, 11, 'Kenya AA Bean', 12000, 1);
+
+-- order_id = 3
+INSERT INTO order_item (order_item_id, orders_id, item_name, price, quantity)
+VALUES (6, 12, 'Drip Filter', 1500, 3),
+       (7, 12, 'Cold Brew Pack', 2200, 2);
+
+-- order_id = 4
+INSERT INTO order_item (order_item_id, orders_id, item_name, price, quantity)
+VALUES (8, 13, 'Home Grinder', 9900, 1);
+
+-- order_id = 5
+INSERT INTO order_item (order_item_id, orders_id, item_name, price, quantity)
+VALUES (9, 14, 'Tumbler 350ml', 2500, 2),
+       (10, 14, 'Tumbler Lid', 2500, 1),
+       (11, 14, 'Gift Card', 2500, 3);
