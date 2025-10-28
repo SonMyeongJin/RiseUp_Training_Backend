edf18c4cb3653de6c6eac85d2acdb02fe67228a4
SonMyeongJin <smj2802@naver.com>
2025-10-28 14:42:37 +0900

    [Feat: Security] 1. Add Injection_Attack Project

---
+++ Injection_Attack/src/main/java/com/example/injection_attack/InjectionAttackApplication.java
@@ -0,0 +1,13 @@
+package com.example.injection_attack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionAttackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjectionAttackApplication.class, args);
+    }
+
+}
---
+++ Injection_Attack/src/test/java/com/example/injection_attack/InjectionAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.injection_attack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionAttackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
