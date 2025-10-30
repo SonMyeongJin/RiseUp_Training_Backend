762c1857348ebcf4c8995fff50390ac6906a5874
SonMyeongJin <smj2802@naver.com>
2025-10-30 10:53:20 +0900

    [Feat: API] 1. Create MoreConReviewAPI Module

---
+++ MoreConReviewAPI/src/main/java/com/example/moreconreviewapi/MoreConReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.moreconreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreConReviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoreConReviewApiApplication.class, args);
+    }
+
+}
